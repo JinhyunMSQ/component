2339aa6acbff18f6baf57221c51233a0e2a61142
JinhyunMSQ <sanul0319@gmail.com>
2024-05-03 20:36:56 +0900

    component2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import Card from "./card";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  
+  box-sizing: border-box;
+
+  width: 100%;
+  height: 100vh;
+
+  padding: 0 10%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.div`
+background-color: #f1c40f;
+font-size: 2rem;
+font-weigh: 600;
+width: 100%;
+`;
+
+const Subtitle = styled.div`
+font-size: 1.5rem;
+font-weigh: 600;`
 
 function App() {
   return (
-    <div div className="App">
-      <div>~ 한줄요약~</div>
-      <div>대충 부제목이라는 뜻subtitle</div>
+    <Wrapper>
+      <Title> ~ 한줄요약 ~</Title>
+      <Subtitle>대충 부제목이라는 뜻</Subtitle>
       <Card subj = "안녕하세요" desc = "반갑습니다"/>
       <Card subj = "안녕하세요1" desc = "1반갑습니다"/>
       <Card subj = "안녕하세요2" desc = "2반갑습니다"/>
       <Card subj = "안녕하세요3" desc = "3반갑습니다"/>
-  </div>
+  </Wrapper>
 )
 }
 
--- src/card.js
+++ src/card.js
@@ -1,9 +1,33 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+margin: 10px 0;
+
+width: 100%;
+heigh: 100px;
+
+border: 5px solid yellowgreen;
+border-radius: 10px;
+
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+`;
+
+const Subj = styled.div`
+    font-size:1.5rem;
+`;
+
+const Desc = styled.div`
+    font-size:1.5rem;
+`;
+
 const Card = ({subj, desc}) => {
     return (
-        <>
+        <Wrapper>
             <div>{subj}</div>
-            <div>{desc}</div>
-        </>
+            <div>❤️{desc}</div>
+        </Wrapper>
     )
 }
 
