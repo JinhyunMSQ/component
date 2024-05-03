a96fc372123cf85355becde0c8799054d678cb2a
JinhyunMSQ <sanul0319@gmail.com>
2024-05-03 20:03:54 +0900

    before

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./card";
 
 function App() {
-    <div className="App">
-      h1
-    </div>
-  );
+  return (
+    <div div className="App">
+      <div>~ 한줄요약~</div>
+      <div>대충 부제목이라는 뜻subtitle</div>
+      <Card subj = "안녕하세요" desc = "반갑습니다"/>
+      <Card subj = "안녕하세요1" desc = "1반갑습니다"/>
+      <Card subj = "안녕하세요2" desc = "2반갑습니다"/>
+      <Card subj = "안녕하세요3" desc = "3반갑습니다"/>
+  </div>
+)
 }
 
 export default App;
---
+++ src/card.js
@@ -0,0 +1,10 @@
+const Card = ({subj, desc}) => {
+    return (
+        <>
+            <div>{subj}</div>
+            <div>{desc}</div>
+        </>
+    )
+}
+
+export default Card;
